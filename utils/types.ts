05da865fdceeb8a399b3e36ec5c3cd6bbb6df569
YogPanjarale <yog.panjarale@gmail.com>
2021-04-29 22:25:50 +0530

    types for person

---
+++ utils/types.ts
@@ -0,0 +1,20 @@
+export const a =""
+const example = {
+    "id": 781855909923127300,
+    "discord_tag": "Rishabh Tiwari#6261",
+    "avatar": null,
+    "trees": 362,
+    "deadtrees": 0,
+    "coins": 8152,
+    "level": 8
+    }
+interface Person {
+    id: number;
+    discord_tag: string;
+    avatar: string | null;
+    trees: number;
+    deadtrees: number;
+    coins: number;
+    level: number; 
+}
+export type { Person}
