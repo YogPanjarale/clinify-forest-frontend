addf9b7724ee7153c97f3cf7c893a872ad3ee528
YogPanjarale <yog.panjarale@gmail.com>
2021-04-30 22:53:00 +0530

    basic leaderboard design

--- components/leaderboard/item.tsx
+++ components/leaderboard/item.tsx
@@ -1,20 +1,32 @@
-import { Person } from '@utils/types';
-import React from 'react'
+import { Person } from "@utils/types";
+import React from "react";
 
 interface MyProps {
-    person:Person
-	index:number
+	person: Person;
+	index: number;
 }
 
-export const PersonItem: React.FC<MyProps> = ({person,index}) => {
-        return (<>
-		{/* <h3>{JSON.stringify(person)}</h3> */}
-		<div>
-			<h2 className="">{index}.{person.discord_tag}</h2>
-			<p><b>Coins: {person.coins}</b></p>
-			<p><b>Trees Planted: {person.trees}</b></p>
-			<p><b>Trees Withered: {person.deadtrees}</b></p>
-		</div>
-        </>);
-}
+export const PersonItem: React.FC<MyProps> = ({ person, index }) => {
+	return (
+		// <div className="space-y-5 m-10 align-middle">
+			// {/* <h3>{JSON.stringify(person)}</h3> */}
+			<div className="space-y-5 m-10 align-middle">
+				<div className="p-2 mx-auto flex flex-col shadow-lg space-y-4  rounded-xl text-center max-w-prose border-white border-2 bg-gray-50 bg-opacity-50 hover:bg-opacity-90 transition font-bold text-gray-700 hover:text-gray-900">
+				<h2 className="text-xl">
+					{index}.{person.discord_tag}
+				</h2>
+				<p>
+					<b>Coins: {person.coins}</b>
+				</p>
+				<p>
+					<b>Trees Planted: {person.trees}</b>
+				</p>
+				<p>
+					<b>Trees Withered: {person.deadtrees}</b>
+				</p>
+				</div>
+			</div>
+		// {/* </div> */}
+	);
+};
 export default PersonItem;
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -2,15 +2,12 @@ import { Person } from "@utils/types";
 import { PersonItem } from "@components/leaderboard/item";
 export default ({ people }: { people: Person[] }) => {
 	return (
-		<section className="text-gray-600 body-font overflow-hidden">
-			<div className="container px-5 py-24 mx-auto">
-				<div className="-my-8 divide-y-2 divide-gray-100">
+				<div className="bg-gradient-to-r from-green-400 to-blue-500 py-10">
 					{people.map((value,index) => {
 						return <PersonItem key={index} index={index+1} person={value}/>
 					})}
+					
 				</div>
-			</div>
-		</section>
 	);
 };
 export async function getServerSideProps({ params }) {
